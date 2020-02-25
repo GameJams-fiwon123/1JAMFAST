f930251eea8ccd99b6d668bd1a4ea1eb0fe09c56
fiwon123 <felipeinoue@yahoo.com.br>
2020-02-25 16:01:35 -0300

    Player Name

---
+++ PlantTogether/Assets/MyGame/Scripts/Player/NameMultiplayer.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameMultiplayer : MonoBehaviour
+{
+
+	PhotonView photonView;
+
+	public Text playerNameText;
+
+	// Start is called before the first frame update
+	void Start() {
+
+		photonView = GetComponent<PhotonView>();
+
+		playerNameText.text = photonView.Owner.NickName;
+	}
+
+}
